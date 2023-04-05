a4d224535a53e651600aa454890cd0f1a51b42ab
primayazadi <127044669+primayazadi@users.noreply.github.com>
2023-04-05 16:40:30 +0700

    Add files via upload

---
+++ DataMahasiswa1302210001_PRAKTIKAN.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+
+
+
+class DataMahasiswa1302210001_PRAKTIKAN
+{
+    public class Mahasiswa
+    {
+        public Biodata nama { get; set; }
+
+        private char gender;
+        private int age;
+        private String address;
+        private string course;
+
+        public int nim { get; set; }
+
+        public string fakultas { get; set; }
+
+        public Mahasiswa() { }
+
+        public Mahasiswa(Biodata nama, char gender, int age, String address, String course)
+        {
+            this.nama = nama;
+            this.gender = gender;
+            this.age = age;
+            this.address = address;
+            this.course = course;
+        }
+    }
+
+    public class Biodata
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+
+        public Biodata() { }
+
+        public Biodata(string depan, string belakang)
+        {
+            this.depan = depan;
+            this.belakang = belakang;
+        }
+    }
+    public void ReadJSON()
+    {
+        string file = "D:\\jurnal\\modul7_group5\\modul7_group5\\jsconfig1.json";
+        string JSON = File.ReadAllText(file);
+        Mahasiswa d = JsonSerializer.Deserialize<Mahasiswa>(JSON);
+        Console.WriteLine($"Nama {d.nama.depan + " " + d.nama.belakang} dengan gender {d.} dari Fakultas {d.fakultas}");
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul7_group5;
+using System;
+using modul7_group5;
+namespace modul7_group5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa1302210001_PRAKTIKAN dataMahasiswa = new DataMahasiswa1302210001_PRAKTIKAN();
+            dataMahasiswa.ReadJSON();
+
+            Console.ReadKey();
+        }
+    }
+}
