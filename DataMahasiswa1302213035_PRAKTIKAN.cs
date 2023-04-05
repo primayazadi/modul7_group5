e8a1d093baa9b17a9398277028b666f43c9ca3df
nadhifaaffiyan <127294844+nadhifaaffiyan@users.noreply.github.com>
2023-04-05 17:29:10 +0700

    menambahkan nama class

---
+++ DataMahasiswa1302213035_PRAKTIKAN.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+
+
+
+class DataMahasiswa1302210001_PRAKTIKAN
+{
+    public void ReadJSON()
+    {
+        Data d = JsonSerializer.Deserialize<Data>(File.ReadAllText("D:\\modul7_group5\\KPL_MODUL7_KEL5\\KPL_MODUL7_KEL5\\DataMahasiswa1302213035_PRAKTIKAN.json"));
+        Console.WriteLine(d.firstName + " " + d.lastName);
+        Console.WriteLine(d.age);
+        Console.WriteLine(d.gender);
+        Console.WriteLine(d.address.streetAddress);
+        Console.WriteLine(d.address.city);
+        Console.WriteLine(d.address.state);
+        foreach (var item in d.courses)
+        {
+            Console.WriteLine(item.code + " " + item.name);
+        }
+
+    }
+    public class Data
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public char gender { get; set; }
+        public int age { get; set; }
+        public Address address { get; set; }
+        public List<Courses> courses { get; set; }
+
+    }
+    public class Address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+    public class Courses
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KPL_MODUL7_KEL5;
+using System;
+using KPL_MODUL7_KEL5;
+namespace KPL_MODUL7_KEL5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa1302213035_PRAKTIKAN dataMahasiswa = new DataMahasiswa1302213035_PRAKTIKAN();
+            dataMahasiswa.ReadJSON();
+
+            Console.ReadKey();
+        }
+    }
+}
